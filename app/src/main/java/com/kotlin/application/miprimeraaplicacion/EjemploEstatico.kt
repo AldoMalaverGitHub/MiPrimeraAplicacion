e686eb9a0312fb345bb25518038500d54a7b9619
AldoMalaverGitHub <almalaver@gmail.com>
2018-10-03 23:01:22 -0500

    Tercera clase

---
+++ app/src/main/java/com/kotlin/application/miprimeraaplicacion/EjemploEstatico.kt
@@ -0,0 +1,12 @@
+package com.kotlin.application.miprimeraaplicacion
+
+class Jugador{
+    companion object {
+        val nombre:String = "Aldo"
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Jugador.nombre)
+    
+}
